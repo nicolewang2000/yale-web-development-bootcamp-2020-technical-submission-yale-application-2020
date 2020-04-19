9530178d70c2e28cbfe3611b4438fc5e9eb87038
Nicole Wang <nicole.wang@yale.edu>
2020-04-19 01:16:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
-
+name = "Nicole"
+age = 19
+favorite_food = "coffee"
+height_in_inches = 67
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
